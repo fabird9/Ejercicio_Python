6d5f90f461736ede452ebcde5d97f131dccfd9cd
Fabian Rodriguez Dominguez <fabird9@gmail.com>
2020-05-23 23:50:44 +0200

    subo ejercicio python

---
+++ ej18.py
@@ -0,0 +1,10 @@
+cadena="Estoy haciendo un examen"
+
+print( cadena[3] )
+print( cadena[-1] )
+print( cadena[1:4] )
+print( cadena[18:] )
+print( cadena[:3] )
+print( cadena[:] )
+print( cadena[2:-1] )
+print( cadena[-2:] )
